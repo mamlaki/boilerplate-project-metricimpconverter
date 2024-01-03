96e33dc23fe03ae4bf534c45c96a007047745d72
Melek Redwan <melekredwan@icloud.com>
2024-01-02 19:36:32 -0700

    General: Added hint modals.

--- public/js/svgInteractions.js
+++ public/js/svgInteractions.js
@@ -10,3 +10,21 @@ svgs.forEach(svg => {
     svgs.forEach(svg => svg.style.animation = '')
   })
 })
+
+// Reveal the respective info modal when an icon is clicked.
+document.addEventListener('DOMContentLoaded', () => {
+  // Open modal when icon is clicked.
+  document.querySelectorAll('.hint-icon').forEach(icon => {
+    icon.addEventListener('click', () => {
+      let modalId = icon.id.replace('icon', 'modal')
+      document.getElementById(modalId).style.display = 'block'
+    })
+  })
+
+  // Close modal when close button is clicked.
+  document.querySelectorAll('.close-button').forEach(button => {
+    button.addEventListener('click', () => {
+      button.parentElement.style.display = 'none'
+    })
+  })
+})
